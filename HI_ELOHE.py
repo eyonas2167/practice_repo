e41d5d3c77bdc401322717dbdddfdbd6104b02e4
Max Kessler <atlanticalmax@gmail.com>
2023-02-14 17:43:35 +0000

    hi elohe

---
+++ HI_ELOHE.py
@@ -0,0 +1,10 @@
+# write an unessarily verbose hello world, to bug Elohe
+
+def verbose_hello_elohe():
+    print("Hello, Elohe!")
+    print("How are you today?")
+    print("I hope you are well.")
+    print("I am a computer program.")
+
+if __name__ == "__main__":
+    verbose_hello_elohe()
